83ae31812b1b3e3b4b7bfe8b481d0347335815c3
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-06-26 00:16:34 +0530

    Insertion_sort

---
+++ JAVA/Insertion_sort/sort1.java
@@ -0,0 +1,28 @@
+//INSERTION SORT
+
+import java.util.*;
+
+public class sort1 {
+    public static void main(String[] args) {
+        int[] ar = { 5, 3, 4, 1, 2 };
+        insertion(ar);
+        System.out.println(Arrays.toString(ar));
+
+    }
+
+    public static void insertion(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) { // i runs upto N-2
+            for (int j = i + 1; j > 0; j--) {
+
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
