2ad3706b8e0fa1fbede817c305af28a60385b662
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-07-06 01:24:57 +0530

    update

--- JAVA/Cycle_sort/test.java
+++ JAVA/Cycle_sort/test.java
@@ -10,15 +10,20 @@ public class test {
         System.out.println(Arrays.toString(ar));
     }
 
-    public static void cycle(int[] arr) {
+   // This code cycles through an array of integers, making sure that each number is in the correct location. It does this by looping through the array and swapping the current number with the number at its correct index. It continues looping through the array until all numbers are in the correct place. The numbers in the array must be between 0 and the length of the array.
+
+public static void cycle(int[] arr) {
+        // Loop over the array
         int i = 0;
         while (i < arr.length) {
-            // int correct = arr[i] - 1;
-            if (arr[i] != i && arr[i] < arr.length) {
+            // If the number at the current index is not in the right place
+            if (arr[i] != i && arr[i] < arr.length && arr[i] >= 0) {
+                // Swap it with the number at its correct index
                 int temp = arr[arr[i]];
                 arr[arr[i]] = arr[i];
                 arr[i] = temp;
             } else {
+                // Otherwise, move to the next index
                 i++;
             }
         }
