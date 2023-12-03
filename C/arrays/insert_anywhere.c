596c8a98b779dc768d91843beafddb769f717cfc
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-12-04 02:31:54 +0530

    Remove unnecessary code for shifting elements in insert_anywhere.c

--- C/arrays/insert_anywhere.c
+++ C/arrays/insert_anywhere.c
@@ -25,10 +25,6 @@ int main()
 
     arr[pos] = ele;
 
-    // //shifting the elements to left
-    // for(int i = pos; i < size; i++)
-    //     arr[i] = arr[i + 1];
-
     printf("elements of array: ");
     for (int i = 0; i < size; i++)
     {
