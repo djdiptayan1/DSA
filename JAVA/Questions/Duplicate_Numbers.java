53993331aed25aee42b0a4be8d4914a4e83d7722
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2024-01-31 21:39:48 +0530

    Add solution for finding duplicate numbers in an array

---
+++ JAVA/Questions/Duplicate_Numbers.java
@@ -0,0 +1,19 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
